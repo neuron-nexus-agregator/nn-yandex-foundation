929ff97b0877cb9b3c1133c6457192b7cf83bb43
neuron-nexus-agregator <gafarov.neuron.nexus@gmail.com>
2025-09-27 14:17:33 +0300

    More examples

--- example/src/main.rs
+++ example/src/main.rs
@@ -5,10 +5,10 @@ use dotenvy::dotenv;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    let prompt = "Dog in sofa";
-    let path = "./image.png";
-    let width_ratio: i64 = 1;
-    let height_ratio: i64 = 1;
+    let prompt = "Кошка сидит на диване";
+    let path = "./images/image4.png";
+    let width_ratio: i64 = 3;
+    let height_ratio: i64 = 2;
     match generate_image(prompt, path, width_ratio, height_ratio).await{
         Ok(_) => println!("Image generated successfully"),
         Err(e) => eprintln!("{e}"),
